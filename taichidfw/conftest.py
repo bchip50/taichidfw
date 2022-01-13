b704be91091b52012cde12b1c50d78efff6bfa43
bchip50 <bill@chipmaninfo.com>
2022-01-12 22:19:49 -0600

    base cookiecutter

---
+++ taichidfw/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from taichidfw.users.models import User
+from taichidfw.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user() -> User:
+    return UserFactory()
